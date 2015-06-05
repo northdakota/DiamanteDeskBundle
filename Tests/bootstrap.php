8787eeddc2c504a3db43567b41d47c32116b73c4
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-06-05 17:53:10 +0300

    fix travis

---
+++ Tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+$autoload = realpath(__DIR__ . getenv('CLASS_AUTOLOADER_FROM_SRC_DIR'));
+
+if (!is_file($autoload)) {
+    realpath(__DIR__ . getenv('CLASS_AUTOLOADER_FROM_VENDOR_DIR'));
+}
+
+if (!is_file($autoload)) {
+    die("Composer Autoload file not found.");
+}
+
+require $autoload;
